b71b24406f8c52cae2101edc941e6666605b9751
razanfawwaz <razanfawwaz1905@gmail.com>
2023-10-26 14:40:57 +0700

    feat: add config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <span>Docs Latex TA FMIPA USK</span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/razanfawwaz/docs-latex-ta",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/razanfawwaz/docs-latex-ta",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Informatika USK @2023",
   },
-}
+};
 
-export default config
+export default config;
